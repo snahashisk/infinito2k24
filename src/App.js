b9aa765493551d96c432c78c430f238664215191
Snahashis <snahashiskanrar@gmail.com>
2023-04-29 12:19:19 +0530

    added smooth scrolling

--- src/App.js
+++ src/App.js
@@ -1,17 +1,60 @@
 import "./App.css";
+import { useRef } from "react";
 import { Home, About, Speaker, Event, Sponsor, Contact } from "./pages";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+
 function App() {
+  const aboutRef = useRef(null);
+  const speakerRef = useRef(null);
+  const eventRef = useRef(null);
+  const sponsorRef = useRef(null);
+  const contactRef = useRef(null);
+
+  const scrollToRef = (ref) => {
+    if (ref && ref.current) {
+      window.scrollTo({
+        top: ref.current.offsetTop,
+        behavior: "smooth",
+      });
+    }
+  };
+
+  const handleScrollToAbout = () => {
+    scrollToRef(aboutRef);
+  };
+
+  const handleScrollToSpeaker = () => {
+    scrollToRef(speakerRef);
+  };
+
+  const handleScrollToEvent = () => {
+    scrollToRef(eventRef);
+  };
+
+  const handleScrollToSponsor = () => {
+    scrollToRef(sponsorRef);
+  };
+
+  const handleScrollToContact = () => {
+    scrollToRef(contactRef);
+  };
+
   return (
     <>
-      <Navbar />
+      <Navbar
+        onAboutClick={handleScrollToAbout}
+        onSpeakerClick={handleScrollToSpeaker}
+        onEventClick={handleScrollToEvent}
+        onSponsorClick={handleScrollToSponsor}
+        onContactClick={handleScrollToContact}
+      />
       <Home />
       <About />
-      <Speaker />
-      <Event />
-      <Sponsor />
-      <Contact />
+      <Speaker ref={speakerRef} />
+      <Event ref={eventRef} />
+      <Sponsor ref={sponsorRef} />
+      <Contact ref={contactRef} />
       <Footer />
     </>
   );
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,51 @@
 import { useState, useEffect } from "react";
 import { BiRightArrowAlt } from "react-icons/bi";
 import festlogo from "../images/fest-logo-2.png";
+
 const Navbar = () => {
   const [scrollY, setScrollY] = useState(0);
+
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
+
+  const scrollToAbout = () => {
+    window.scrollTo({
+      top: window.innerHeight,
+      behavior: "smooth",
+    });
+  };
+
+  const scrollToSpeaker = () => {
+    window.scrollTo({
+      top: 2 * window.innerHeight,
+      behavior: "smooth",
+    });
+  };
+
+  const scrollToEvent = () => {
+    window.scrollTo({
+      top: 3 * window.innerHeight,
+      behavior: "smooth",
+    });
+  };
+
+  const scrollToSponsor = () => {
+    window.scrollTo({
+      top: 4.65 * window.innerHeight,
+      behavior: "smooth",
+    });
+  };
+
+  const scrollToContact = () => {
+    window.scrollTo({
+      top: 6 * window.innerHeight,
+      behavior: "smooth",
+    });
+  };
 
   useEffect(() => {
     function handleScroll() {
@@ -20,33 +63,53 @@ const Navbar = () => {
   } duration-150 hover:bg-opacity-100 ${scrollY > 0 ? "py-4" : "py-6"}`;
   return (
     <nav className={navbarClasses}>
-      <img src={festlogo} className="h-16" />
+      <img
+        src={festlogo}
+        className="h-16 cursor-pointer"
+        onClick={scrollToTop}
+      />
       <div>
         <ul className="flex gap-8 text-gray-300 items-center">
-          <li className="hover:border-b-2 border-yellow-300 hover:text-yellow-300 hover:scale-105 duration-100 cursor-pointer">
+          <li
+            className="hover:border-b-2 border-yellow-300 hover:text-yellow-300 hover:scale-105 duration-100 cursor-pointer"
+            onClick={scrollToTop}
+          >
             HOME
           </li>
-          <li className="hover:border-b-2 border-yellow-300 hover:text-yellow-300 hover:scale-105 duration-100 cursor-pointer">
+          <li
+            className="hover:border-b-2 border-yellow-300 hover:text-yellow-300 hover:scale-105 duration-100 cursor-pointer"
+            onClick={scrollToAbout}
+          >
+            ABOUT
+          </li>
+          <li
+            className="hover:border-b-2 border-yellow-300 hover:text-yellow-300 hover:scale-105 duration-100 cursor-pointer"
+            onClick={scrollToSpeaker}
+          >
             SPEAKER
           </li>
-          <li className="hover:border-b-2 border-yellow-300 hover:text-yellow-300 hover:scale-105 duration-100 cursor-pointer">
+          <li
+            className="hover:border-b-2 border-yellow-300 hover:text-yellow-300 hover:scale-105 duration-100 cursor-pointer"
+            onClick={scrollToEvent}
+          >
             EVENT
           </li>
-          <li className="hover:border-b-2 border-yellow-300 hover:text-yellow-300 hover:scale-105 duration-100 cursor-pointer">
-            SCHEDULE
-          </li>
-          <li className="hover:border-b-2 border-yellow-300 hover:text-yellow-300 hover:scale-105 duration-100 cursor-pointer">
+          <li
+            className="hover:border-b-2 border-yellow-300 hover:text-yellow-300 hover:scale-105 duration-100 cursor-pointer"
+            onClick={scrollToSponsor}
+          >
             SPONSOR
           </li>
-          <li className="hover:border-b-2 border-yellow-300 hover:text-yellow-300 hover:scale-105 duration-100 cursor-pointer">
+          <li
+            className="hover:border-b-2 border-yellow-300 hover:text-yellow-300 hover:scale-105 duration-100 cursor-pointer"
+            onClick={scrollToContact}
+          >
             CONTACT
           </li>
           <li>
             <button
               className="bg-yellow-400 text-black py-2 px-6 rounded-full hover:scale-110 duration-150 flex items-center gap-1 hover:shadow-lg font-semibold"
-              onClick={() =>
-                window.open("https://forms.gle/xDKUna9HhdM3qftb7", "_blank")
-              }
+              onClick={scrollToEvent}
             >
               REGISTER NOW
               <BiRightArrowAlt className="text-2xl" />
