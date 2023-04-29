148dcb8a3fb9a63e7b402f27ce29acf73c8ea8fb
Snahashis <snahashiskanrar@gmail.com>
2023-04-29 11:16:10 +0530

    added logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import { BiRightArrowAlt } from "react-icons/bi";
-
+import festlogo from "../images/fest-logo-2.png";
 const Navbar = () => {
   const [scrollY, setScrollY] = useState(0);
 
@@ -17,12 +17,10 @@ const Navbar = () => {
 
   const navbarClasses = `fixed font-sans z-10 w-full flex justify-around items-center bg-black ${
     scrollY > 0 ? " bg-opacity-100" : " bg-opacity-50"
-  } duration-150 hover:bg-opacity-100 ${scrollY > 0 ? "py-4" : "py-8"}`;
+  } duration-150 hover:bg-opacity-100 ${scrollY > 0 ? "py-4" : "py-6"}`;
   return (
     <nav className={navbarClasses}>
-      <h1 className="font-bold text-5xl text-white">
-        TECH<span className="text-yellow-400">FEST</span>
-      </h1>
+      <img src={festlogo} className="h-16" />
       <div>
         <ul className="flex gap-8 text-gray-300 items-center">
           <li className="hover:border-b-2 border-yellow-300 hover:text-yellow-300 hover:scale-105 duration-100 cursor-pointer">
